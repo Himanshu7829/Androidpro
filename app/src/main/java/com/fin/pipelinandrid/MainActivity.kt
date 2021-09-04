b4d06464d30408a560c2b09c8235de3dc87cd017
Sharma, Himanshu <Himanshu.Sharma@united.com>
2021-09-04 21:32:43 +0530

    [Himanshu][App center config]

--- app/src/main/java/com/fin/pipelinandrid/MainActivity.kt
+++ app/src/main/java/com/fin/pipelinandrid/MainActivity.kt
@@ -2,11 +2,19 @@ package com.fin.pipelinandrid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "99c0a88d-577b-413d-8d94-3fd26a139a5b",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 
     override fun onResume() {
